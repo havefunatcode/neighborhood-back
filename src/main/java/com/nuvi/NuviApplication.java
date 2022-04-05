61c83a3c4105a383eed98807f7d6defef0a63f92
kakjzi <anmern3@naver.com>
2022-04-05 12:39:04 +0900

    ADD: envFile

---
+++ src/main/java/com/nuvi/NuviApplication.java
@@ -0,0 +1,13 @@
+package com.nuvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuvi/NuviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
